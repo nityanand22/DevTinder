702fd7636f57788116002aa029cffcb9e71c8dcd
Nityanand <you@example.com>
2025-02-27 23:06:19 +0530

    tested get and post method in postman using use method

--- src/app.js
+++ src/app.js
@@ -2,14 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from the home server ");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from the server hello");
-});
-
 app.use("/test", (req, res) => {
   res.send("Hello");
 });
