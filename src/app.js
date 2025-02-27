b1199c9308ad3d3d65c3a3591a280157af12fe93
Nityanand <you@example.com>
2025-02-27 23:38:59 +0530

    we have seen the use of + * and ?

--- src/app.js
+++ src/app.js
@@ -2,20 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ name: "Nityanand", age: 25 });
+app.get("/ab?c", (req, res) => {
+  //* it will work for /ac and /abc means b becomes optional
+  res.send({ name: "Nick", lastname: "Doe" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("User created successfully");
+app.get("/ab+c", (req, res) => {
+  //* it will work for /abc and /abbbbbb....c means b can be repeated
+  res.send({ name: "Nick", lastname: "Doe" });
+});
+
+app.get("/ab*cd", (req, res) => {
+  //* it will work for /abcd, /abxyzcd, /ab123cd means anything can come between ab and cd
+  res.send({ name: "Nick", lastname: "Doe" });
+});
+
+app.get("/a(bc)?d", (req, res) => {
+  //* it will work for /ad and /abcd means bc is optional
+  res.send({ name: "Nick", lastname: "Doe" });
+});
+
+app.get("/a(bc)+d", (req, res) => {
+  //* it will work for /abcd, /abcbcd, /abcbcbcbcd means bc can be repeated
+  res.send({ name: "Nick", lastname: "Doe" });
 });
 
 app.delete("/user", (req, res) => {
   res.send("User deleted successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello");
 });
 
 app.listen(3000, () => {
