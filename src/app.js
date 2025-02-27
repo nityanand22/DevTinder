dfa84927097028febaa364305a6b2cb50b926bc4
Nityanand <you@example.com>
2025-02-27 23:56:00 +0530

    created routes of diff routes

--- src/app.js
+++ src/app.js
@@ -2,33 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ab?c", (req, res) => {
-  //* it will work for /ac and /abc means b becomes optional
-  res.send({ name: "Nick", lastname: "Doe" });
+app.get("/user/:userId/:name/:password", (req, res) => {
+  res.send(req.params);
 });
 
-app.get("/ab+c", (req, res) => {
-  //* it will work for /abc and /abbbbbb....c means b can be repeated
-  res.send({ name: "Nick", lastname: "Doe" });
+app.get("/user/:userId", (req, res) => {
+  res.send(req.params);
+  res.send({ name: "Nick", lastname: "jones" });
 });
 
-app.get("/ab*cd", (req, res) => {
-  //* it will work for /abcd, /abxyzcd, /ab123cd means anything can come between ab and cd
+app.get("/user", (req, res) => {
+  res.send(req.query);
   res.send({ name: "Nick", lastname: "Doe" });
-});
-
-app.get("/a(bc)?d", (req, res) => {
-  //* it will work for /ad and /abcd means bc is optional
-  res.send({ name: "Nick", lastname: "Doe" });
-});
-
-app.get("/a(bc)+d", (req, res) => {
-  //* it will work for /abcd, /abcbcd, /abcbcbcbcd means bc can be repeated
-  res.send({ name: "Nick", lastname: "Doe" });
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User deleted successfully");
 });
 
 app.listen(3000, () => {
