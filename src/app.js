ed8c68be8d23f3f2746519499ba30b846ffd7471
Nityanand <you@example.com>
2025-02-28 19:39:57 +0530

    used the next() function

--- src/app.js
+++ src/app.js
@@ -2,19 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  res.send(req.params);
-});
-
-app.get("/user/:userId", (req, res) => {
-  res.send(req.params);
-  res.send({ name: "Nick", lastname: "jones" });
-});
-
-app.get("/user", (req, res) => {
-  res.send(req.query);
-  res.send({ name: "Nick", lastname: "Doe" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("hii ");
+    // res.send("Response 1"); // This will send the response and will not call the next middleware
+    // and if we want the reponse from the next handler then we cant send the response from the first handler
+    next();
+  },
+  (req, res, next) => {
+    // res.send("REsponse2");
+    console.log("hello");
+    next();
+  },
+  (req, res) => {
+    console.log("hello world");
+    res.send("Response 3");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
