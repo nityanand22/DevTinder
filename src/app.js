a54db76df1a3b6a0c22c91493423407c8cb8941b
Nityanand <you@example.com>
2025-03-01 23:40:29 +0530

    gracefully handled the error

--- src/app.js
+++ src/app.js
@@ -1,29 +1,16 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-//! here we have two routes, one for getting all data and one for deleting user, and both routes have same code, so we can create a middleware to handle all get post ,... requests
-
-// Middleware
-app.use("/admin", adminAuth);
-
-app.post("/user/login", (req, res) => {
-  res.send("User login successfully");
+app.get("/getUserData", (req, res) => {
+  throw new Error("error");
+  res.send("User data sent");
 });
 
-//* here i have used same code for both the routes, so i will create a middleware for this, and so we need middleware for this
-
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("This is user route111");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("All Data deleted");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(3000, () => {
