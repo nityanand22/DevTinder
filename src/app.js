5a499d0a2b3ac5447006c4d6686f9cda18282a9a
Nityanand <you@example.com>
2025-03-01 19:28:15 +0530

    see why we need middleware by a demo authorization

--- src/app.js
+++ src/app.js
@@ -2,23 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", [
-  (req, res, next) => {
-    console.log("hii ");
-    // res.send("Response 1"); // This will send the response and will not call the next middleware
-    // and if we want the reponse from the next handler then we cant send the response from the first handler
-    next();
-  },
-  (req, res, next) => {
-    // res.send("Response2");
-    console.log("hello");
-    next();
-  },
-  (req, res) => {
-    console.log("hello world");
-    res.send("Response 3");
-  },
-]);
+app.get("/admin/getAllData", (req, res, next) => {
+  // const token = req.body?.token;
+  // const token = "admin";
+  const token = "abcd";
+  const isAdmin = token === "admin";
+  if (isAdmin) {
+    res.send("All data");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
+
+app.get("/admin/deleteUser", (req, res, next) => {
+  // const token = req.body?.token;
+  // const token = "admin";
+  const token = "abcd";
+  const isAdmin = token === "admin";
+  if (isAdmin) {
+    res.send("All data");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
