75389549ee6a228d944a1d7db7ea0ed9247546ad
Nityanand <you@example.com>
2025-03-12 14:39:40 +0530

    just added the token expires time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const app = express();
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
@@ -8,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("./middlewares/auth");
 
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -44,7 +44,9 @@ app.post("/login", async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (isPasswordValid) {
       // created JWT token
-      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder");
+      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder", {
+        expiresIn: "1d",
+      });
       // Add the token to cookie and send the response back to user
       res.cookie("token", token);
       res.send("Login Successfully");
@@ -66,65 +68,9 @@ app.get("/profile", userAuth, async (req, res) => {
   }
 });
 
-// Get User by email
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.email;
-  try {
-    const user = await User.findOne({ email: userEmail });
-    if (user.length === 0) {
-      res.status(404).send("user not found");
-    } else {
-      res.send(user);
-    }
-  } catch (error) {
-    res.status(400).send("can't find data");
-  }
-});
-
-// Feed API - GET/feed  - get all the users from the database
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {}
-});
-
-// Delete a User
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const user = await User.findByIdAndDelete(userId);
-    res.send("User deleted successfully");
-  } catch (error) {
-    res.status(400).send("can't find data");
-  }
-});
-
-// Update data of a user
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = ["photoUrl", "about", "skills", "age", "gender"];
-
-    const isUpdatesAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdatesAllowed) {
-      throw new Error("Updates not allowed");
-    }
-    if (data?.skills?.length > 10) {
-      throw new Error("Skills can't be more than 10");
-    }
-    await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    res.send("user updated");
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  res.send(user.firstName + " sent the connection request");
 });
 
 connectDB()
