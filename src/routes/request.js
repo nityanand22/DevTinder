36dbbdffb002410b5067ffd1443dbc1382951629
Nityanand <you@example.com>
2025-03-13 13:30:10 +0530

    ** establish connection intern level

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,10 +2,32 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 
 const requestRouter = express.Router();
+const ConnectionRequest = require("../models/connectionRequest");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  res.send(user.firstName + " sent the connection request");
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+      res.status(200).json({
+        message: "Connection Request sent successfully",
+        data,
+      });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+);
 
 module.exports = requestRouter;
