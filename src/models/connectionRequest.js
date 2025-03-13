216f24a324381f6244b0d0ba40434b31f08bb8ad
Nityanand <you@example.com>
2025-03-13 12:39:17 +0530

    used enum and created connection schema

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const connectionRequestModel = mongoose.model(
+  "connectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = connectionRequestModel;
